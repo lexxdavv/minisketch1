d45590a4c3ccd293eb2a56b198f337592f5b1020
lexxdavv <97611548+lexxdavv@users.noreply.github.com>
2022-01-24 16:44:57 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,30 @@
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(800, 400);
   }
   
   function draw() {
-    background(220);
+    background(100,220,0);
+    strokeWeight(4);
+    stroke(0,0,255);
+    fill(0,100,255);
+    circle(400,200,40);    
+
+    strokeWeight(3);
+    stroke(255,0,255);
+    fill(110,255,0);
+    square(200,100,20);
+
+    
+    noFill();
+    strokeWeight(1);
+    stroke(0,110,255);
+    circle(0,0,20);
+    
+    ellipse(200,150,55,55);
+
+    arc(50,55,50,50,0, HALF_PI);
+    noFill();
+    arc(50,55,60,60,HALF_PI, PI);
+    arc(50,55,70,70,PI,PI + QUARTER_PI);
+    arc(50,55,80,80,PI+QUARTER_PI,TWO_PI);
   }
